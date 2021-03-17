a7f7d71fef632271c5515975f5e379bdc45fed20
gkalms <georgina.kalms@gmail.com>
2021-03-17 23:03:22 +1100

    ruby tutorial wed 17th

---
+++ Ruby/ruby_wed1.rb
@@ -0,0 +1,14 @@
+
+
+puts 'Hello there, and what\'s your first name?'
+firstname = gets.chomp
+puts 'What\'s your second name?'
+secondname = gets.chomp
+puts 'What\'s your last name?'
+lastname = gets.chomp
+puts 'Pleased to meet you, ' + firstname + ' ' + secondname + ' ' + lastname + '.'
+
+puts 'Hi, what\'s your favourite number?'
+number = gets.chomp
+number2 = number + 1
+puts 'number2 'is a bigger and better number''
